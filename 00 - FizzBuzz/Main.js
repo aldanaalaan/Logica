11f24860014c55988bba020608c83ea38a7b8269
Alan Aldana <alanrodrigoyaelaldana@gmail.com>
2023-11-28 13:32:15 -0600

    Codigo de programa a funciones

--- 00 - FizzBuzz/Main.js
+++ 00 - FizzBuzz/Main.js
@@ -6,15 +6,16 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
-
-for (let i = 1; i <= 100; i++) {
-  if (i % 5 == 0 && i % 3 == 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 == 0) {
-    console.log("Fizz");
-  } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+function FizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
   }
 }
--- 01 - L33T/Main.js
+++ 01 - L33T/Main.js
@@ -7,54 +7,40 @@
  *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
  */
 
-// Importaciones
-const readline = require("readline");
+function Leet(cadena) {
+  let leetstr = "";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+  let leet = {
+    a: "4",
+    b: "13",
+    c: "[",
+    d: ")",
+    e: "3",
+    f: "|=",
+    g: "&",
+    h: "#",
+    i: "1",
+    j: ",_|",
+    k: "|<",
+    l: "1",
+    m: "/\\/\\",
+    n: "^/",
+    o: "0",
+    p: "|*",
+    q: "(_,)",
+    r: "12",
+    s: "5",
+    t: "7",
+    u: "(_)",
+    v: "\\/",
+    w: "\\/\\/",
+    x: "><",
+    y: "j",
+    z: "2",
+  };
 
-let cadena = "";
-let car = "";
-
-let leet = {
-  a: "4",
-  b: "13",
-  c: "[",
-  d: ")",
-  e: "3",
-  f: "|=",
-  g: "&",
-  h: "#",
-  i: "1",
-  j: ",_|",
-  k: "|<",
-  l: "1",
-  m: "/\\/\\",
-  n: "^/",
-  o: "0",
-  p: "|*",
-  q: "(_,)",
-  r: "12",
-  s: "5",
-  t: "7",
-  u: "(_)",
-  v: "\\/",
-  w: "\\/\\/",
-  x: "><",
-  y: "j",
-  z: "2",
-};
-
-rl.question("Dame una cadena: ", (res) => {
-  cadena = res.toLowerCase();
-  rl.close();
-});
-
-rl.on("close", () => {
   for (let i = 0; i < cadena.length; i++) {
-    car = leet[cadena.charAt(i)] || cadena.charAt(i);
-    process.stdout.write(car);
+    leetstr += leet[cadena.charAt(i)] || cadena.charAt(i);
   }
-});
+  return leetstr;
+}
